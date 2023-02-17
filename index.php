938c1317ae8555c36d33ad549233d04431e97925
Juan Vidal <juanmvg2003@gmail.com>
2023-02-17 18:36:51 -0500

    página de productos hecha

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
     <div class="banner">
         <div class="banner-text-container">
             <h1 class="banner-title">Inicio</h1>
-            <h5 class="banner-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cumque, amet deserunt quas dicta dignissimos ullam nihil aspernatur repellendus laudantium qui distinctio sapiente ea, praesentium blanditiis doloribus totam quia molestiae.</h5>
-            <a class="btn" href="contact.php">Contacto!</a>
-        </div>
+            <h5 class="banner-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cumque, amet deserunt quas dicta dignissimos ulla. Lorem ipsum dolor sit amet.</h5>
+            <a class="banner-btn" href="products.php">Ver Productos</a>
     </div>
-    
+
 <?php include('./template/footer.php')?>
--- products.php
+++ products.php
@@ -1,4 +1,81 @@
 <!-- Incluyendo header y footer -->
 <?php include('./template/header.php')?>
-    <h1>Productos</h1>
+
+
+    <div class="principal-container-products">
+
+        <!-- banner -->
+        <div class="banner-products-container">
+            <h1 class="banner-products-title">Productos</h1>
+        </div>
+
+
+        <!-- contenedor de productos -->
+        <div class="products-container">
+
+            <div class="card-product-container">
+                
+                <div class="product-content-container">
+                    <div class="product-image-container">
+                        <img src="" alt="Image">
+                    </div>
+                    <div class="product-text-container">
+                        <h5 class="product-title">Producto 1</h5>
+                        <p class="product-text">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, debitis?</p>
+                    </div>
+                    <a class="btn-product" href="#">Ver más</a>
+                </div>
+            
+            </div>
+
+            <div class="card-product-container">
+                
+                <div class="product-content-container">
+                    <div class="product-image-container">
+                        <img src="" alt="Image">
+                    </div>
+                    <div class="product-text-container">
+                        <h5 class="product-title">Producto 2</h5>
+                        <p class="product-text">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, debitis?</p>
+                    </div>
+                    <a class="btn-product" href="#">Ver más</a>
+                </div>
+            
+            </div>
+
+            <div class="card-product-container">
+                
+                <div class="product-content-container">
+                    <div class="product-image-container">
+                        <img src="" alt="Image">
+                    </div>
+                    <div class="product-text-container">
+                        <h5 class="product-title">Producto 3</h5>
+                        <p class="product-text">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, debitis?</p>
+                    </div>
+                    <a class="btn-product" href="#">Ver más</a>
+                </div>
+            
+            </div>
+
+            <div class="card-product-container">
+                
+                <div class="product-content-container">
+                    <div class="product-image-container">
+                        <img src="" alt="Image">
+                    </div>
+                    <div class="product-text-container">
+                        <h5 class="product-title">Producto 4</h5>
+                        <p class="product-text">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, debitis?</p>
+                    </div>
+                    <a class="btn-product" href="#">Ver más</a>
+                </div>
+            
+            </div>
+
+        </div>
+
+        
+    </div>
+
 <?php include('./template/footer.php')?>
