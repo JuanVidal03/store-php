5ff3045174470996c37acb657bb3771868cb2f78
Juan Vidal <juanmvg2003@gmail.com>
2023-02-21 15:33:35 -0500

    interfaz login creada

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingresar</title>
+    <!-- css -->
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    
+    <div class="login-container-page">
+        <div class="login-container">
+
+            <h5>Ingresa!</h5>
+            <p>Ingresa tus credenciales</p>
+            
+            <form class="login-form" action="">
+                <input type="text" name="user"  placeholder="Usuario">
+                <input type="password" name="pass"  placeholder="Contraseña">
+                <input class="login-submit" type="submit" value="Ingresar">
+            </form>
+
+        </div>
+    </div>
+
+</body>
+</html>
